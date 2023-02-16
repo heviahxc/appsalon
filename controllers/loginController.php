19c08dceae5ff4d234e3a33fbb1c296f16da04b6
heviahxc <heviamc@gmail.com>
2023-02-16 17:54:07 -0300

    first commit

---
+++ controllers/loginController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Controllers;
+
+class LoginController{
+    public static function login(){
+        echo "login";
+    }
+    public static function logout(){
+        echo "logout";
+    }
+    public static function olvide(){
+        echo "olvide";
+    }
+    public static function recuperar(){
+        echo "recuperar";
+    }
+    public static function crear(){
+        echo "recuperar";
+    }
+}
